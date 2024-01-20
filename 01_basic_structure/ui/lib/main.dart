5b7bdaff57a71228186aa1055ebab6046252900d
milochen0418 <milo_chen@mail2000.com.tw>
2024-01-21 02:24:28 +0800

    bug: add image , it work on Chrome but not on macOS

--- 01_basic_structure/ui/lib/main.dart
+++ 01_basic_structure/ui/lib/main.dart
@@ -1,22 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Hello World Flutter App!!'),
+          title: const Text('Hello World Flutter App!!'),
         ),
         body: Center(
-          child: Text(
-            'Hello, World~~',
-            style: TextStyle(fontSize: 24),
+          child: Column(
+            mainAxisSize: MainAxisSize.min, // 將 Column 的大小設為最小
+            children: [
+              const Text(
+                'Hello, World~~',
+                style: TextStyle(fontSize: 24),
+              ),
+              const SizedBox(height: 20), // 添加一些空間
+              Image.network(
+                'https://picsum.photos/250?image=9',
+              ),
+            ],
           ),
         ),
       ),
