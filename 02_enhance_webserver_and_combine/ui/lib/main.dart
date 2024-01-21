e16d25d7bee4ce43d0f94d15d873fb50a8c130cb
milochen0418 <milo_chen@mail2000.com.tw>
2024-01-21 21:36:40 +0800

    feat: flutter client click button to get random string from server

--- 02_enhance_webserver_and_combine/ui/lib/main.dart
+++ 02_enhance_webserver_and_combine/ui/lib/main.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _randomString = '';
+
+  void _getRandomString() async {
+    final response = await http.get(Uri.parse('http://localhost:8080/randomString'));
+    if (response.statusCode == 200) {
+      setState(() {
+        _randomString = response.body;
+      });
+    } else {
+      print('Failed to load random string');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,6 +36,7 @@ class MyApp extends StatelessWidget {
         ),
         body: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             mainAxisSize: MainAxisSize.min, // 將 Column 的大小設為最小
             children: [
               const Text(
@@ -27,6 +47,12 @@ class MyApp extends StatelessWidget {
               Image.network(
                 'https://picsum.photos/250?image=9',
               ),
+              ElevatedButton(
+                onPressed: _getRandomString,
+                child: const Text('Get Server BFF Random'),
+              ),
+              const SizedBox(height: 20),
+              Text(_randomString),
             ],
           ),
         ),
