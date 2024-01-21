7dfc02becded5629516a78bb326fe1cf143c40b2
milochen0418 <milo_chen@mail2000.com.tw>
2024-01-21 21:12:29 +0800

    add api randomString

--- 02_enhance_webserver_and_combine/server/lib/routes.dart
+++ 02_enhance_webserver_and_combine/server/lib/routes.dart
@@ -1,9 +1,18 @@
+import 'dart:math';
 import 'package:shelf/shelf.dart';
 
 Response routesHandler(Request request) {
   if (request.url.path == 'hello') {
     return Response.ok('Hello, world!');
+  } else if (request.url.path == 'randomString') {
+    var randomString = _generateRandomString(16);
+    return Response.ok(randomString);
   }
-
   return Response.notFound('Not Found');
+}
+
+String _generateRandomString(int length) {
+  const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  final random = Random();
+  return String.fromCharCodes(Iterable.generate(length, (_) => characters.codeUnitAt(random.nextInt(characters.length))));
 }
