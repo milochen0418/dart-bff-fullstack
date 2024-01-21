a61ba40cbd0f76673663eba87b9c3fdea93291a3
milochen0418 <milo_chen@mail2000.com.tw>
2024-01-21 21:58:51 +0800

    fix: Chrome cannot run well when get http but macOS is okay. because it need a cors processing for Chrome device

--- 02_enhance_webserver_and_combine/server/lib/server.dart
+++ 02_enhance_webserver_and_combine/server/lib/server.dart
@@ -1,9 +1,11 @@
 import 'package:shelf/shelf.dart';
 import 'package:startserver/routes.dart';
+import 'middleware/cors_middleware.dart'; // 引入 CORS middleware 
 
 Handler createServer() {
   var handler = const Pipeline()
       .addMiddleware(logRequests())
+      .addMiddleware(corsMiddleware()) // 添加 CORS middleware
       .addHandler(routesHandler);
 
   return handler;
